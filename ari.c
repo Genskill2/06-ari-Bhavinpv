86bdfbea03c9255a05c619cde4290df83713d66a
Bhavinpv <84716754+Bhavinpv@users.noreply.github.com>
2021-06-29 15:04:09 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -1,58 +1,70 @@
-#include<stdio.h>
-#include<cs50.h>
-#include <string.h>
 #include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <cs50.h>
 
+string ari (string para) {
+	
+	int no_of_characters(string );
+	int no_of_words(string );
+	int no_of_sentences(string );
+	int score_calculator(int , int , int );
+	
+	int char_count = no_of_characters(para);
+	int word_count = no_of_words(para);
+	int sentence_count = no_of_sentences(para);
+	int score = score_calculator(char_count, word_count, sentence_count);
+	
+	string grade[14] = {"Kindergarten", "First/Second Grade", "Third Grade", "Fourth Grade", "Fifth Grade", "Sixth Grade", "Seventh Grade", "Eight Grade", "Ninth Grade", "Tenth Grade", "Eleventh Grade" , "Twelfth Grade", "College Student", "Professor"};
+	if (score > 14) {
+		return grade[13];
+	}
+	else if (score == 0) {
+		return 0;
+	}
+	else {
+		return grade[score - 1];
+	}
+}
 
-string ari(string para){
- float score_calculator(int,int,int);
- int alpnum=0 , words=0 , sents=0;
+//functions
+int no_of_characters(string para) {
+	int characters = 0;
+	for (int i = 0; i < strlen(para) ; i++) {
+		if(isalnum(para[i])) {
+			characters++;
+		}
+	}
+	return characters;
+}
 
- for (int i=0 ; i<strlen(para); i++) {
-  if(isalnum(para[i]) == 1) {
-     alpnum++ ;
-      }
-      }
-  for (int i=0 ; i<strlen(para); i++) {
-   if( para[i] ==' ') {
-   words++ ;
-   }
-   }
-   for (int i=0 ; i<strlen(para); i++) {
-  if(para[i]=='.' || para[i]=='?' || para[i]=='!') {
-  sents++;
-  }
-  }
- 
- 
- int rounded_score;
- float score= score_calculator(alpnum,words,sents);
-  
- if(score - (int)score !=0) {
-    rounded_score=(int)(score + 1 );
- } 
- else{
-  rounded_score=(int)score;
- }
- 
-  string grade[14]={"Kindergarten","First/Second Grade" , "Third Grade","Fourth Grade","Fifth Grade","Sixth Grade","Seventh Grade"," Eighth Grade","Ninth Grade","Tenth Grade","Eleventh Grade","Twelfth grade","College student","Professor"};
- 
- if(rounded_score <= 14) {
-   return grade[rounded_score - 1] ;
- }
- else if(rounded_score==0){
- return "NULL";
- }
- else
-  return grade[13];
-  }
-  
-  float score_calculator(int characters,int words , int sentences){
-  float score= 4.71*(float)(characters/words) + 0.5*(float)(words/sentences) - 21.43;
-  return score;
-  }
-  
+int no_of_words(string para) {
+	int words = 0;
+	for (int i = 0; i < strlen(para) ; i++) {
+		if(para[i] == ' ') {
+			words++;
+		}
+	}
+	return words;
+}
 
+int no_of_sentences(string para) {
+	int sentences = 0;
+	for (int i = 0; i < strlen(para) ; i++) {
+		if((para[i]) == '.' || para[i] == '?' || para[i] == '!') {
+			sentences++;
+		}
+	}
+	return sentences;
+}
 
-
-
+int score_calculator(int characters, int words , int sentences) {
+	float score = 4.71 * (float)characters/words + 0.5 * (float)words/sentences - 21.43;
+	
+	if (score - (int)score != 0) {                       
+		score = (int)score + 1;
+	}
+	return score;
+}
